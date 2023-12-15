ed26affc0780e7f0e2d3e5ebff2e1fda9ab78ede
JacobNeitzell <jake.neitzell@hotmail.com>
2023-12-14 20:53:25 -0700

    Changed leagues to where to buy

--- pages/leagues/index.vue
+++ pages/leagues/index.vue
@@ -3,7 +3,146 @@
 </script>
 
 <template>
-  <div />
+  <div class="min-h-screen bg-gradient-to-tr from-blue-800 to-green-200 flex justify-center items-center py-20">
+    <div class="md:px-4 md:grid md:grid-cols-2 lg:grid-cols-3 gap-5 space-y-4 md:space-y-0">
+      <div class="max-w-sm bg-white px-6 pt-6 pb-2 rounded-xl shadow-lg transform hover:scale-105 transition duration-500">
+        <h3 class="mb-3 text-xl font-bold text-indigo-600">
+          Beginner Friendly
+        </h3>
+        <div class="relative">
+          <img class="w-full rounded-xl" src="https://i5.walmartimages.com/seo/Westside-Discs-Origio-Burst-Disc-Golf-Starter-Set-Beginners-Frisbee-Sets-Include-King-Distance-Driver-Underworld-Fairway-Warship-Midrange-Crown-Putte_2d5fb089-051f-441f-80f2-74d64b892c29.e7bbf65531f71b741a10971d6d5bb697.jpeg" alt="Colors">
+          <p class="absolute top-0 bg-yellow-300 text-gray-800 font-semibold py-1 px-3 rounded-br-lg rounded-tl-lg">
+            $59.99
+          </p>
+        </div>
+        <h1 class="mt-4 text-gray-800 text-2xl font-bold cursor-pointer">
+          Westside Discs
+        </h1>
+        <div class="my-4">
+          <div class="flex space-x-1 items-center">
+            <span>
+              <!-- <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-indigo-600 mb-1.5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> -->
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
+              <!-- </svg> -->
+            </span>
+            <!-- <p>AVG: $59.99</p> -->
+          </div>
+          <div class="flex space-x-1 items-center">
+            <span>
+              <!-- <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-indigo-600 mb-1.5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> -->
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 4v12l-4-2-4 2V4M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
+              <!-- </svg> -->
+            </span>
+            <p>4 discs and disc marker</p>
+          </div>
+          <div class="flex space-x-1 items-center">
+            <span>
+              <!-- <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-indigo-600 mb-1.5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> -->
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 20l4-16m4 4l4 4-4 4M6 16l-4-4 4-4" />
+              <!-- </svg> -->
+            </span>
+            <p>Westside Discs</p>
+          </div>
+          <!-- TODO add link to shop sites or Add another button -->
+          <button class="mt-4 text-xl w-full text-white bg-indigo-600 py-2 rounded-xl shadow-lg">
+            Where to buy
+          </button>
+        </div>
+      </div>
+      <div class="max-w-sm bg-white px-6 pt-6 pb-2 rounded-xl shadow-lg transform hover:scale-105 transition duration-500">
+        <h3 class="mb-3 text-xl font-bold text-indigo-600">
+          Beginner Friendly
+        </h3>
+        <div class="relative">
+          <img class="w-full rounded-xl" src="https://target.scene7.com/is/image/Target/GUEST_cc5f78fe-c37b-44cc-a814-44ce8f86ad39?wid=488&hei=488&fmt=pjpeg" alt="Colors">
+          <p class="absolute top-0 bg-yellow-300 text-gray-800 font-semibold py-1 px-3 rounded-br-lg rounded-tl-lg">
+            $49.99
+          </p>
+          <!-- <p class="absolute top-0 right-0 bg-yellow-300 text-gray-800 font-semibold py-1 px-3 rounded-tr-lg rounded-bl-lg">
+            %20 Discount
+          </p> -->
+        </div>
+        <h1 class="mt-4 text-gray-800 text-2xl font-bold cursor-pointer">
+          Dynamic discs Beginner set
+        </h1>
+        <div class="my-4">
+          <!-- <div class="flex space-x-1 items-center">
+            <span>
+              <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-indigo-600 mb-1.5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
+              </svg>
+            </span>
+            <p>1:34:23 Minutes</p>
+          </div> -->
+          <div class="flex space-x-1 items-center">
+            <span>
+              <!-- <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-indigo-600 mb-1.5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> -->
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 4v12l-4-2-4 2V4M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
+              <!-- </svg> -->
+            </span>
+            <p>3 Discs</p>
+          </div>
+          <div class="flex space-x-1 items-center">
+            <span>
+              <!-- <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-indigo-600 mb-1.5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> -->
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 20l4-16m4 4l4 4-4 4M6 16l-4-4 4-4" />
+              <!-- </svg> -->
+            </span>
+            <p>Dynamic Discs</p>
+          </div>
+          <button class="mt-4 text-xl w-full text-white bg-indigo-600 py-2 rounded-xl shadow-lg">
+            Where to buy
+          </button>
+        </div>
+      </div>
+      <div class="max-w-sm bg-white px-6 pt-6 pb-2 rounded-xl shadow-lg transform hover:scale-105 transition duration-500">
+        <h3 class="mb-3 text-xl font-bold text-indigo-600">
+          Beginner Friendly
+        </h3>
+        <div class="relative">
+          <img class="w-full rounded-xl" src="https://www.dynamicdiscs.com/cdn/shop/products/8909-2.jpg?v=1624913240" alt="Colors">
+          <p class="absolute top-0 bg-yellow-300 text-gray-800 font-semibold py-1 px-3 rounded-br-lg rounded-tl-lg">
+            $29.99
+          </p>
+        </div>
+        <h1 class="mt-4 text-gray-800 text-2xl font-bold cursor-pointer">
+          Latitude Beginner Set
+        </h1>
+        <div class="my-4">
+          <div class="flex space-x-1 items-center">
+            <span>
+              <!-- <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-indigo-600 mb-1.5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> -->
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
+              <!-- </svg> -->
+            </span>
+            <!-- <p>1:34:23 Mi</p> -->
+          </div>
+          <div class="flex space-x-1 items-center">
+            <span>
+              <!-- <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-indigo-600 mb-1.5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> -->
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 4v12l-4-2-4 2V4M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
+              <!-- </svg> -->
+            </span>
+            <p>3 Parts</p>
+          </div>
+          <div class="flex space-x-1 items-center">
+            <span>
+              <!-- <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-indigo-600 mb-1.5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> -->
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 20l4-16m4 4l4 4-4 4M6 16l-4-4 4-4" />
+              <!-- </svg> -->
+            </span>
+            <p>Vanilla JS</p>
+          </div>
+          <button class="mt-4 text-xl w-full text-white bg-indigo-600 py-2 rounded-xl shadow-lg">
+            Buy Lesson
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="min-h-screen bg-gradient-to-tl from-green-200 to-blue-800 flex justify-center items-center py-20">
+    <p />
+  </div>
 </template>
 
 <style>
