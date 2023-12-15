2bea25ab8263ddee1cf43a59a9673e27be254cd0
JacobNeitzell <jake.neitzell@hotmail.com>
2023-12-15 16:06:18 -0700

    Shop page has info

--- pages/leagues/index.vue
+++ pages/leagues/index.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-  <div class="min-h-screen bg-gradient-to-tr from-blue-800 to-green-200 flex justify-center items-center py-20">
+  <div class="min-h-screen  flex justify-center items-center py-20">
     <div class="md:px-4 md:grid md:grid-cols-2 lg:grid-cols-3 gap-5 space-y-4 md:space-y-0">
       <div class="max-w-sm bg-white px-6 pt-6 pb-2 rounded-xl shadow-lg transform hover:scale-105 transition duration-500">
         <h3 class="mb-3 text-xl font-bold text-indigo-600">
@@ -45,10 +45,14 @@
           </div>
           <!-- TODO add link to shop sites or Add another button -->
           <button class="mt-4 text-xl w-full text-white bg-indigo-600 py-2 rounded-xl shadow-lg">
-            Where to buy
+            McU Sports
+          </button>
+          <button class="mt-4 text-xl w-full text-white bg-indigo-600 py-2 rounded-xl shadow-lg">
+            Gem Discs
           </button>
         </div>
       </div>
+      <!-- NOTE break -->
       <div class="max-w-sm bg-white px-6 pt-6 pb-2 rounded-xl shadow-lg transform hover:scale-105 transition duration-500">
         <h3 class="mb-3 text-xl font-bold text-indigo-600">
           Beginner Friendly
@@ -91,10 +95,14 @@
             <p>Dynamic Discs</p>
           </div>
           <button class="mt-4 text-xl w-full text-white bg-indigo-600 py-2 rounded-xl shadow-lg">
-            Where to buy
+            McU Sports
+          </button>
+          <button class="mt-4 text-xl w-full text-white bg-indigo-600 py-2 rounded-xl shadow-lg">
+            Gem Discs
           </button>
         </div>
       </div>
+      <!-- NOTE Break -->
       <div class="max-w-sm bg-white px-6 pt-6 pb-2 rounded-xl shadow-lg transform hover:scale-105 transition duration-500">
         <h3 class="mb-3 text-xl font-bold text-indigo-600">
           Beginner Friendly
@@ -134,14 +142,64 @@
             <p>Vanilla JS</p>
           </div>
           <button class="mt-4 text-xl w-full text-white bg-indigo-600 py-2 rounded-xl shadow-lg">
-            Buy Lesson
+            McU Sports
+          </button>
+          <button class="mt-4 text-xl w-full text-white bg-indigo-600 py-2 rounded-xl shadow-lg">
+            Gem Discs
           </button>
         </div>
       </div>
     </div>
   </div>
-  <div class="min-h-screen bg-gradient-to-tl from-green-200 to-blue-800 flex justify-center items-center py-20">
-    <p> test</p>
+  <!-- NOTE Break here from discs to buy and where to buy -->
+  <div class="min-h-screen  flex justify-center items-center py-20">
+    <div class="sm:mb-10 lg:grid lg:grid-cols-5 md:grid-cols-none md:bg-gray-300 bg-gray-300 lg:bg-white lg:h-full">
+      <div class=" px-10 py-10 max-w-md m-auto lg:col-span-2 mt-20 mb-20 shadow-xl rounded-xl lg:mt-10 md:shadow-xl md:rounded-xl lg:shadow-none lg:rounded-none lg:w-full lg:mb-10 lg:px-5 lg:pt-5 lg:pb-5 lg:max-w-lg bg-white">
+        <img class="h-10" src="https://scontent.fboi1-1.fna.fbcdn.net/v/t39.30808-6/274691853_336748685057277_3509229795474566954_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=efb6e6&_nc_ohc=dhRjagznP-0AX90GAt5&_nc_ht=scontent.fboi1-1.fna&oh=00_AfAIp4fW5bw_7mqbFXxvdpAvxpCPLtmM0f7sghErzCK5Dg&oe=65827E2A" alt="Workcation logo">
+        <img class="h-64 sm:h-52 sm:w-full sm:object-cover lg:hidden object-center mt-2 rounded-lg shadow-2xl" src="https://gemdiscs.com/cdn/shop/files/multi_grey_Croped_2000x.png?v=1646603499" alt="Ad- woman on a beach">
+        <h1 class="mt-5 font-bold text-lg lg:mt-7">
+          Gem Discs
+        </h1>
+        <h1 class="font-bold text-lg text-gray-600">
+          Get your discs here and shop local
+        </h1>
+        <h1 class="text-lg text-gray-600 text-justify pt-2">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        </h1>
+        <button class="mt-5 bg-gray-600 p-3 shadow-2xl rounded-xl text-white font-bold hover:bg-gray-800">
+          Shop Here
+        </button>
+      </div>
+
+      <div class="hidden relative lg:block  lg:col-span-3">
+        <img class="absolute inset-0 w-full h-full object-cover object-center" src="https://scontent.fboi1-1.fna.fbcdn.net/v/t39.30808-6/405249811_730915195724751_5474229757708083756_n.png?_nc_cat=107&ccb=1-7&_nc_sid=783fdb&_nc_ohc=_PAgG9-RMkcAX_zEoQU&_nc_ht=scontent.fboi1-1.fna&oh=00_AfDve2oYgkuJBokhk-yin0GSjYJRCiCRQ8RDxNsfYKXB8Q&oe=6580E05E" alt="Gem Disc Site">
+      </div>
+    </div>
+  </div>
+  <!-- NOTE MCU Sports starts here -->
+  <div class="min-h-screen  flex justify-center items-center py-20">
+    <div class="sm:mb-10 lg:grid lg:grid-cols-5 md:grid-cols-none md:bg-gray-300 bg-gray-300 lg:bg-white lg:h-full">
+      <div class=" px-10 py-10 max-w-md m-auto lg:col-span-2 mt-20 mb-20 shadow-xl rounded-xl lg:mt-10 md:shadow-xl md:rounded-xl lg:shadow-none lg:rounded-none lg:w-full lg:mb-10 lg:px-5 lg:pt-5 lg:pb-5 lg:max-w-lg bg-white">
+        <img class="h-10" src="https://i0.wp.com/mcusports.com/wp-content/uploads/2019/03/float.png" alt="Workcation logo">
+        <img class="h-64 sm:h-52 sm:w-full sm:object-cover lg:hidden object-center mt-2 rounded-lg shadow-2xl" src="https://gemdiscs.com/cdn/shop/files/multi_grey_Croped_2000x.png?v=1646603499" alt="Ad- woman on a beach">
+        <h1 class="mt-5 font-bold text-lg lg:mt-7">
+          MCU Sports
+        </h1>
+        <h1 class="font-bold text-lg text-gray-600">
+          Get your discs here and shop local
+        </h1>
+        <h1 class="text-lg text-gray-600 text-justify pt-2">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        </h1>
+        <button class="mt-5 bg-gray-600 p-3 shadow-2xl rounded-xl text-white font-bold hover:bg-gray-800">
+          Shop Here
+        </button>
+      </div>
+
+      <div class="hidden relative lg:block  lg:col-span-3">
+        <img class="absolute inset-0 w-full h-full object-cover object-center" src="https://scontent.fboi1-1.fna.fbcdn.net/v/t39.30808-6/316197800_575937644536068_6153363969177137364_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=783fdb&_nc_ohc=eB41UWlXRPIAX_kn0df&_nc_ht=scontent.fboi1-1.fna&oh=00_AfCVjbA5Ik4MrP78j6sEFdYSlDgK3pEtoegixoK2od9vxQ&oe=6582315D" alt="Gem Disc Site">
+      </div>
+    </div>
   </div>
 </template>
 
