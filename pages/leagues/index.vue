bee509674ed05668c7e20f1f9b20b9135eea878f
JacobNeitzell <jake.neitzell@hotmail.com>
2023-12-15 19:06:01 -0700

    adjusted heights

--- pages/leagues/index.vue
+++ pages/leagues/index.vue
@@ -179,7 +179,7 @@
         </div>
       </div>
     </div>
-
+    <!-- NOTE gem state discs  -->
     <div class="min-h-min  flex justify-center items-center py-20">
       <div class="sm:mb-10 lg:grid lg:grid-cols-5 md:grid-cols-none md:bg-gray-300 bg-gray-300 lg:bg-white lg:h-full">
         <div class=" px-10 py-10 max-w-md m-auto lg:col-span-2 mt-20 mb-20 shadow-xl rounded-xl lg:mt-10 md:shadow-xl md:rounded-xl lg:shadow-none lg:rounded-none lg:w-full lg:mb-10 lg:px-5 lg:pt-5 lg:pb-5 lg:max-w-lg bg-white">
